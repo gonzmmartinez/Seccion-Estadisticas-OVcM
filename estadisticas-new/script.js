af76c9eba689c23c1a109e314eaa69c88e21548a
Gonzalo M. Martinez - OVcM <gonzmmartinez@gmail.com>
2025-01-14 12:23:55 -0300

    Cambios

--- estadisticas-new/script.js
+++ estadisticas-new/script.js
@@ -9,12 +9,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Estado inicial de la barra según el dispositivo
     if (isMobile) {
-        sidebar.classList.add('hidden'); // Oculta la barra en móviles
+        sidebar.classList.add('collapsed'); // Oculta la barra en móviles
         toggleButton.style.color = '#3A1D51'; // Color del botón cuando la barra está oculta
         mainContent.classList.add('expanded'); // Expande el contenido principal
         toggleButton.style.left = '5%'; // Ajusta la posición del botón
     } else {
-        sidebar.classList.remove('hidden'); // Muestra la barra en escritorio
+        sidebar.classList.remove('collapsed'); // Muestra la barra en escritorio
         mainContent.classList.remove('expanded'); // Restaura el margen del contenido principal
         toggleButton.style.left = '15%'; // Ajusta la posición del botón
     }
@@ -27,18 +27,16 @@ function toggleSidebar() {
     const toggleButton = document.getElementById('toggleSidebar');
 
     // Alterna la visibilidad de la barra lateral
-    sidebar.classList.toggle('hidden');
+    sidebar.classList.toggle('collapsed');
     mainContent.classList.toggle('expanded');
 
     // Verifica si estamos en un dispositivo móvil
     const isMobile = window.innerWidth <= 800;
 
     // Ajusta la posición y color del botón según el estado de la barra lateral
-    if (sidebar.classList.contains('hidden')) {
-        toggleButton.style.color = '#3A1D51'; // Color del botón cuando la barra está oculta
-        toggleButton.style.left = isMobile ? '5%' : '10px'; // Posición según el dispositivo
+    if (sidebar.classList.contains('collapsed')) {
+        toggleButton.style.left = isMobile ? '5%' : '1.25%'; // Posición según el dispositivo
     } else {
-        toggleButton.style.color = '#fafafa'; // Color del botón cuando la barra está visible
         toggleButton.style.left = isMobile ? '5%' : '15%'; // Posición según el dispositivo
     }
 }
